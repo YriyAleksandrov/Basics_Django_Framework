cb486ab2ac8e7a2d375de87ee12eb04f45051e82
Юрий <Aleksandrovyriy@ya.ru>
2021-05-25 21:48:31 +0300

    add task_8

--- geekshop/mainapp/views.py
+++ geekshop/mainapp/views.py
@@ -4,6 +4,7 @@ from django.shortcuts import render
 from django.shortcuts import get_object_or_404
 from basketapp.models import Basket
 from mainapp.models import ProductCategory, Product
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 
 
 def get_basket(user):
@@ -23,7 +24,7 @@ def get_same_products(hot_products):
     return same_products
 
 
-def products(request, pk=None):
+def products(request, pk=None, page=1):
     title = 'продукты'
     category = ''
     products = ''
@@ -34,10 +35,22 @@ def products(request, pk=None):
     if pk is not None:
         if pk == 0:
             products = Product.objects.all().order_by('price')
-            category = {'name': 'все'}
+            category = {
+                'name': 'все',
+                'pk': 0
+            }
         else:
             category = get_object_or_404(ProductCategory, pk=pk)
             products = Product.objects.filter(category_id__pk=pk).order_by('price')
+
+        paginator = Paginator(products, 2)
+
+        try:
+            products_paginator = paginator.page(page)
+        except PageNotAnInteger:
+            products_paginator = paginator.page(1)
+        except EmptyPage:
+            products_paginator = paginator.page(paginator.num_pages)
 
     hot_product = get_hot_product()
     same_products = get_same_products(hot_product)
@@ -46,7 +59,7 @@ def products(request, pk=None):
         'title': title,
         'categories': categories,
         'category': category,
-        'products': products,
+        'products': products_paginator,
         'basket': basket,
         'hot_product': hot_product,
         'same_products': same_products,
